c2ec5b205c97ee04fe1e41d9cd0eebb0cd71cbaa
spicyseaweed <94048668+spicyseaweed@users.noreply.github.com>
2022-05-21 23:14:25 +0800

    some changes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,62 @@
+
 <template>
-  <AboutPage/>
-  <LayoutPage/>
+  <div class="app-wrapper">
+    <div class="app">
+      <navigationBar />
+      <router-view />
+      <Footer />
 
-
+    </div>
+  </div>
 </template>
 
 <script>
-import AboutPage from './views/About/about.vue'
-import LayoutPage from './layout/index.vue'
+//import AboutPage from './views/AboutPage/About.vue'
+//import LayoutPage from './layout/index.vue'
+import navigationBar from './components/Navigation.vue'
+import Footer from './components/Footer.vue'
 //import SearchMainPage from './views/Search-Main/index.vue'
 
 export default {
   name: 'App',
   components: {
-    AboutPage, LayoutPage
-  }
+    //AboutPage,
+    //LayoutPage,
+    navigationBar,
+    Footer,
+
+}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang = "scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand", sans-serif;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+.link {
+  cursor: pointer;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: black;
+}
+
+.link-light {
+  color: #fff;
 }
 </style>
+
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,24 +1,205 @@
 <template>
 <header>
-    <nav class = "container">
-        <div class = "branding">
-            <router-link class = "header" :to = "{name : 'Home'}">BOOKit@NUS</router-link>
-        </div>
-        <div class = "nav-links">
-            <ul>
-                <router-links class = "link" to = "#will update">Home</router-links>
-                <router-links class = "link" to = "#will update">Book Facilities</router-links>
-                <router-links class = "link" to = "#will update">Forum</router-links>
-                <router-links class = "link" to = "#will update">User</router-links>
-                <router-links class = "link" to = "#will update">Login/Register</router-links>
-            </ul>
+    <nav>
+        <div class = "container">
+            <div class = "branding">
+                <router-link class="header" :to="{ name: 'home' }">BOOKit@NUS</router-link>
+            </div>
+            <div class = "nav-links">
+                <ul>
+                    <router-link class = "link" to = "#">Home</router-link>
+                    <router-link class = "link" to = "#">Book Facilities</router-link>
+                    <router-link class = "link" to = "#">Forum</router-link>
+                    <router-link class = "link" to = "#">User</router-link>
+                    <router-link class = "link" to = "#">Login/Register</router-link>
+                </ul>
+            </div>
         </div>
     </nav>
+
 </header>
 </template>
 
 <script>
-export default {};
+
+export default {
+    name: 'navigationBar'
+};
 </script>
 
-<style lang = "scss" scoped></style>
+
+<style lang="scss" scoped>
+header {
+  background-color: rgba(237, 209, 96, 0.669);
+  padding: 0 25px;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  z-index: 99;
+}
+
+.container {
+  display: flex;
+  padding: 25px 0;
+
+  .branding {
+    display: flex;
+    align-items: center;
+    .header {
+      font-size: 24px;
+      color: #000;
+      text-decoration: none;
+      font-weight: 600;
+    }
+    .logo {
+       max-width: 20px;
+       max-height: 10px;
+    }
+  }
+
+  .nav-links {
+    position: relative;
+    min-height: 40px;
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    ul {
+      margin-right: 32px;
+      display: none;
+      @media (min-width: 500px) {
+        display: initial;
+      }
+
+      .link {
+        margin-right: 32px;
+      }
+
+      .link:last-child {
+        margin-right: 0;
+      }
+    }
+
+    .link {
+      color: black;
+      &:hover {
+        color: #253db3;
+      }
+    }
+  }
+
+  .profile {
+    position: relative;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: #fff;
+    background-color: #303030;
+
+    span {
+      pointer-events: none;
+    }
+
+    .profile-menu {
+      top: 60px;
+      right: 0px;
+      position: absolute;
+      width: 250px;
+      background-color: #303030;
+      box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+      span {
+        position: absolute;
+        right: 10px;
+        top: -10px;
+        border-left: 10px solid transparent;
+        border-right: 10px solid transparent;
+        border-bottom: 10px solid #303030;
+      }
+
+      .info {
+        display: flex;
+        align-items: center;
+        padding: 15px;
+        border-bottom: 1px solid #fff;
+
+        .initials {
+          position: initial;
+          width: 40px;
+          height: 40px;
+          background-color: #fff;
+          color: #303030;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border-radius: 50%;
+        }
+
+        div {
+          flex: 1;
+          margin-left: 24px;
+        }
+
+        p:nth-child(1) {
+          font-size: 14px;
+        }
+
+        p:nth-child(2),
+        p:nth-child(3) {
+          font-size: 12px;
+        }
+      }
+
+      .options {
+        padding: 15px;
+        .option {
+          text-decoration: none;
+          color: #fff;
+          display: flex;
+          align-items: center;
+          margin-bottom: 12px;
+
+          .icon {
+            width: 18px;
+            height: auto;
+          }
+
+          i {
+            font-size: 18px;
+          }
+
+          p {
+            font-size: 14px;
+            margin-left: 12px;
+          }
+        }
+        .option:last-child {
+          margin-bottom: 0px;
+        }
+      }
+    }
+
+    img {
+      width: 35px;
+      height: 35px;
+    }
+  }
+}
+
+.link {
+  text-decoration: none;
+  color: #fff;
+  font-weight: 500;
+  padding: 0 8px;
+  text-transform: uppercase;
+  transition: 300ms color ease;
+
+  &:hover {
+    color: #1eb8b8;
+  }
+}
+
+</style>
