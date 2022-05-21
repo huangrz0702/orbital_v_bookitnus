44986beec010efe1a6a0fec9baf278f8509c85d3
spicyseaweed <94048668+spicyseaweed@users.noreply.github.com>
2022-05-21 12:49:17 +0800

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,7 +2,16 @@
 <header>
     <nav class = "container">
         <div class = "branding">
-            <router-link class = "header" : to = "{name : 'Home'}"></router-link>
+            <router-link class = "header" :to = "{name : 'Home'}">BOOKit@NUS</router-link>
+        </div>
+        <div class = "nav-links">
+            <ul>
+                <router-links class = "link" to = "#will update">Home</router-links>
+                <router-links class = "link" to = "#will update">Book Facilities</router-links>
+                <router-links class = "link" to = "#will update">Forum</router-links>
+                <router-links class = "link" to = "#will update">User</router-links>
+                <router-links class = "link" to = "#will update">Login/Register</router-links>
+            </ul>
         </div>
     </nav>
 </header>
