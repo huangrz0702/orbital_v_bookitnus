8a28612b65d9342dd20af8fc48e8941dd4925b15
huangrz0702 <e0774177@u.nus.edu>
2022-06-14 18:50:41 +0800

    initial commit

---
+++ dist/js/241.e5facc6b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkorbital_v_bookitnus"]=self["webpackChunkorbital_v_bookitnus"]||[]).push([[241],{7241:function(n,e,o){o.r(e),o.d(e,{default:function(){return c}});var t=o(6252);function r(n,e,o,r,u,a){return(0,t.wg)(),(0,t.iD)("p",null,"welcome to admin home")}var u={name:"AdHomePage"},a=o(3744);const i=(0,a.Z)(u,[["render",r]]);var c=i}}]);
+//# sourceMappingURL=241.e5facc6b.js.map
